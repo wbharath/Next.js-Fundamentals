ae7f59593a8378f082d7b80421c795e45de83e4c
wbharath <wbharath51@gmail.com>
2025-10-09 21:30:03 -0300

    middleware done

--- app/components/UsersList.tsx
+++ app/components/UsersList.tsx
@@ -6,12 +6,12 @@ async function UsersList() {
   return (
     <div className="mt-4">
       {users.length ? (
-        <div>
+        <div className="max-w-lg">
           {users.map((user) => {
             return (
               <h4
                 key={user.id}
-                className="capitalize text-lgflex justify-between items-center mb-2"
+                className="capitalize text-lg flex justify-between items-center mb-2"
               >
                 {user.firstName} {user.lastName}
                 <DeleteButton id={user.id} />
--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:'https',
-                hostname:'www.course-api.com',
-                port:'',
-                pathname:'/images/**',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.course-api.com',
+        port: '',
+        pathname: '/images/**'
+      }
+    ]
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+}
 
-            },
-        ],
-    },
-};
-
-export default nextConfig;
+export default nextConfig
